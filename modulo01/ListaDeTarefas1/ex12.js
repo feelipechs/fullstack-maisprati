95d2eaa5985d7acb5b7e5d30545887833a9dedfd
Felipe Chagas <155638523+feelipechs@users.noreply.github.com>
2024-04-19 20:51:01 -0300

    Add files via upload

---
+++ modulo01/ListaDeTarefas1/ex12.js
@@ -0,0 +1,7 @@
+// DIVISIVEIS POR 11 COM RESTO 5
+
+for (let i = 1000; i <= 1999; i++) {
+    if (i % 11 == 5) {
+        console.log(i)
+    }
+}
---
+++ modulo01/ListaDeTarefas1/ex3.js
@@ -0,0 +1,15 @@
+// OPERAÇÕES MATEMÁTICAS
+
+const prompt = require('prompt-sync')();
+
+let num1 = parseInt(prompt('Insira o primeiro valor: '));
+let num2 = parseInt(prompt('Insira o segundo valor: '));
+let num3 = parseInt(prompt('Insira o terceiro valor: '));
+let num4 = parseInt(prompt('Insira o quarto valor: '));
+
+n1 = num1 + 25;
+n2 = num2 * 3;
+n3 = num3 * 0.12
+num4 = num1 + num2 + num3
+
+console.log(`Valor 1: ${n1} \nValor 2: ${n2} \nValor 3: ${n3} \nValor 4: ${num4}`)
