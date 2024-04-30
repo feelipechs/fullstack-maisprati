7083915d18821cb7af054d521c2d813792b8b4af
Chagas <155638523+feelipechs@users.noreply.github.com>
2024-04-29 21:40:38 -0300

    Delete fullstack-maisprati/modulo01/ListaDeTarefas1/ex1.js

--- fullstack-maisprati/modulo01/ListaDeTarefas1/ex1.js
+++
@@ -1,8 +0,0 @@
-// CONVERSOR CELSIUS PARA FAHRENHEIT
-
-const prompt = require('prompt-sync')();
-
-let celsius = prompt('Insira a temperatura em ºCelsius que deseja converter: ');
-let fahrenheit = celsius * 1.8 + 32
-
-console.log(`${celsius}ºC equivalem a ${fahrenheit}ºF`)
