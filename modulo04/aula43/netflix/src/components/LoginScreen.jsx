625fa593df23c2ac373dbb680e52ba04fe3a4a6f
feelipechs <flpchagas@outlook.com>
2024-07-29 18:42:22 -0300

    att

--- modulo04/aula43/netflix/src/components/LoginScreen.jsx
+++ modulo04/aula43/netflix/src/components/LoginScreen.jsx
@@ -1,10 +1,16 @@
 import styled from 'styled-components'
 
 const Container = styled.div`
+    background-image: url('./imgs/background.jpg');
+    /* background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat; */
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    width: 100vw;
+    height: 100vh;
 `
 
 // const Header = styled.header`
@@ -26,7 +32,7 @@ function LoginScreen() {
     return (
         <Container>
             <header>
-                <img src="./assets/logo.png" alt="" />
+                <img src="./imgs/logo.png" alt="logo" />
             </header>
             <main>
                 <Login>
@@ -39,9 +45,10 @@ function LoginScreen() {
                     <p>OU</p>
                     <button>Usar um código de acesso</button>
                     <p>Esqueceu a senha?</p>
-                    <input type="checkbox" id="horns" name="horns" />
-                    <label for="horns">Horns</label>
-                    <p>Novo por aqui? Assine agora.</p>
+                    <input type="checkbox" id="" name="" />
+                    <label htmlFor="">Lembre-se de mim</label>
+                    <p>Novo por aqui? <a href="#">Assine Agora</a></p>
+                    <p>Esta página é protegida pelo Google reCAPTCHA para garantir que você não é um robô. <a href="#">Saiba Mais</a></p>
                 </Login>
             </main>
             {/* <input type="checkbox" id="horns" name="horns" />
